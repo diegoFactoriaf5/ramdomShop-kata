6c0cbd94adda74d26b2b53e8faa286fbade53418
Ruji7576 <ukrainemaksymhryshchuk@gmail.com>
2024-06-19 22:39:19 +0200

    complited_tests_from_original_code

--- src/main/java/org/example/ShoppingCart.java
+++ src/main/java/org/example/ShoppingCart.java
@@ -6,14 +6,14 @@ import java.util.List;
 
 public class ShoppingCart {
 
-    private final List<Product> products = new ArrayList<>();
+    private final List<Product> productsList = new ArrayList<>();
 
     public void addProduct(Product product) {
-        products.add(product);
+        productsList.add(product);
     }
 
     public Double getTotalPrice() {
-        return  products.stream()
+        return  productsList.stream()
                 .map(this::calculatePrice)
                 .reduce(BigDecimal.ZERO, BigDecimal::add)
                 .doubleValue();
--- src/test/java/org/example/ShoppingCartTest.java
+++ src/test/java/org/example/ShoppingCartTest.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShoppingCartTest {
@@ -28,16 +30,50 @@ class ShoppingCartTest {
         assertEquals(shoppingCart.getTotalPrice(), 5.0);
 
     }
+    @Test
+    void calculatePriceForMagicCards_green() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, null, false, "green", null, "Magic: The Gathering - Lightning Bolt", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 4.4);
+
+    }
+    @Test
+    void calculatePriceForMagicCards_black() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, null, false, "black", null, "Magic: The Gathering - Lightning Bolt", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 6.8);
+
+    }
+    @Test
+    void calculatePriceForMagicCards_default() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, null, false, "other", null, "Magic: The Gathering - Lightning Bolt", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 2.0);
+
+    }
+
 
     @Test
     void calculatePriceForWine() {
         ShoppingCart shoppingCart = new ShoppingCart();
 
-        Product product = new Product(null, 5, false, null, null, "el tio juanillo", null);
+        Product product = new Product(null, 10, false, null, null, "el tio juanillo", null);
 
         shoppingCart.addProduct(product);
 
-        assertEquals(shoppingCart.getTotalPrice(), 100.0);
+        assertEquals(shoppingCart.getTotalPrice(), 200.0);
 
     }
 
@@ -52,4 +88,75 @@ class ShoppingCartTest {
         assertEquals(shoppingCart.getTotalPrice(), 50.0);
 
     }
+    @Test
+    void calculatePriceForFish_blue() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        BigDecimal bigDecimal = new BigDecimal("0.10");
+
+        Product product = new Product(null, null, false, "blue", bigDecimal, null, null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 0.20);
+}
+    @Test
+    void calculatePriceForFish_gold() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        BigDecimal bigDecimal = new BigDecimal("100.0");
+
+        Product product = new Product(null, null, false, "gold", bigDecimal, null, null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 10000.0);
+    }
+    @Test
+    void calculatePriceForFish_default() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        BigDecimal bigDecimal = new BigDecimal("0.0");
+
+        Product product = new Product(null, null, false, "other", bigDecimal, null, null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 0.0);
+    }
+    @Test
+    void calculatePriceFor_Black_Lotus() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, null, false, null,null, "Magic: The Gathering - Black Lotus", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 40000.0);
+    }
+    @Test
+    void calculatePriceFor_animals_terrestrial() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(4, null, false, null,null, null, null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 16.8);
+    }
+
+    @Test
+    void calculatePriceFor_chocolate() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        BigDecimal bigDecimal = new BigDecimal("1.0");
+
+        Product product = new Product(null, null, false,"black", bigDecimal,null,null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 1.0);
+    }
+
+
 }
