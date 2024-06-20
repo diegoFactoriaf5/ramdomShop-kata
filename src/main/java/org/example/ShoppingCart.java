e8ad468f47b432be8dd763867a024cf7079e48d9
Ruji7576 <ukrainemaksymhryshchuk@gmail.com>
2024-06-20 23:21:09 +0200

    refactor

--- src/main/java/org/example/ShoppingCart.java
+++ src/main/java/org/example/ShoppingCart.java
@@ -20,59 +20,114 @@ public class ShoppingCart {
     }
 
     private BigDecimal calculatePrice(Product product) {
-        if (product.getNumberOfLegs() != null) {
-            if(product.getName() != null) {
-                if (product.getName().equals("Spiders")) {
-                    double priceOflegs = product.getNumberOfLegs() * 1.20;
-                    double multiplic;
-                    if (product.isStinky()) {
-                        multiplic = 0.5;
-                    } else {
-                        multiplic = 1;
-                    }
-                    return switch (product.getColor()) {
-                        case "gold" -> BigDecimal.valueOf((3 + priceOflegs) * multiplic);
-                        case "red" -> BigDecimal.valueOf((2 + priceOflegs) * multiplic);
-                        default -> BigDecimal.valueOf(priceOflegs * multiplic);
-                    };
-                }
-            }
+        if(calculateAnimalPrice(product) !=null) {
+            return calculateAnimalPrice(product);
+        }
+        if (calculateNotAnimalPrice(product) !=null) {
+            return calculateNotAnimalPrice(product);
+        }
+        return product.getSellPrice();
+    }
+
+    private static BigDecimal calculateAnimalPrice(Product product) {
+        if (calculateTerrestrialPrice(product) != null) {
+            return calculateTerrestrialPrice(product);
+        }
+        if (countSpidersPrice(product) != null) {
+            return countSpidersPrice(product);
+        }
+        if (countFishPrice(product) != null) {
+            return countFishPrice(product);
+        }
+        return null;
+    }
+
+    private static BigDecimal calculateNotAnimalPrice(Product product) {
+        if (countGourmetPrice(product) != null) {
+            return countGourmetPrice(product);
+        }
+        if (countLotusPrice(product) != null) {
+            return countLotusPrice(product);
+        }
+        if(countCardsPrice(product) !=null) {
+            return countCardsPrice(product);
+        }
+        return null;
+    }
+
+    private static BigDecimal calculateTerrestrialPrice(Product product) {
+        if (product.getNumberOfLegs() != null  && !"Spiders".equals(product.getName())) {
             return BigDecimal.valueOf(4.2 * product.getNumberOfLegs());
-        } else if (product.getAge() != null && !product.getName().startsWith("Magic: The Gathering")) {
+        }
+        return null;
+    }
+
+    private static BigDecimal countSpidersPrice(Product product) {
+        if (product.getNumberOfLegs() != null && product.getName().equals("Spiders")) {
+                double legsPrice = product.getNumberOfLegs() * 1.20;
+                return switch (product.getColor()) {
+                    case "gold" -> BigDecimal.valueOf((3 + legsPrice) / divideCards(product));
+                    case "red" -> BigDecimal.valueOf((2 + legsPrice) / divideCards(product));
+                    default -> BigDecimal.valueOf(legsPrice / divideCards(product));
+                };
+            }
+        return null;
+    }
+
+    private static BigDecimal countGourmetPrice(Product product) {
+        if (product.getAge() != null && !product.getName().startsWith("Magic: The Gathering")) {
             if (product.isStinky()) {
-                return BigDecimal.valueOf(10.0* product.getAge());
+                return BigDecimal.valueOf(10.0 * product.getAge());
             } else {
                 return BigDecimal.valueOf(20.0 * product.getAge());
             }
-        } else if (product.getColor() != null && product.getBasePrice() != null) {
+        }
+        return null;
+    }
+
+    private static BigDecimal countFishPrice(Product product) {
+        if (product.getColor() != null && product.getBasePrice() != null) {
             return switch (product.getColor()) {
                 case "blue" -> product.getBasePrice().add(BigDecimal.valueOf(0.1));
                 case "gold" -> product.getBasePrice().multiply(BigDecimal.valueOf(100.0));
                 default -> product.getBasePrice();
             };
-        } else if (product.getName().equals("Magic: The Gathering - Black Lotus")) {
+        }
+        return null;
+    }
+
+    private static BigDecimal countLotusPrice(Product product) {
+        if (product.getName().equals("Magic: The Gathering - Black Lotus")) {
             return BigDecimal.valueOf(40000.0);
-        } else if (product.getName().startsWith("Magic: The Gathering")) {
-            double blueRed = 1;
-            double blackGreen = 1;
-            if (product.getAge() != null) {
-                if (product.getAge() >= 10) {
-                    blueRed = 2;
-                }
-                if (product.getAge() >= 20) {
-                    blackGreen = 1.2;
-                }
-            }
+        }
+        return null;
+    }
+
+    private static BigDecimal countCardsPrice(Product product) {
+        if (product.getName().startsWith("Magic: The Gathering")) {
             return switch (product.getColor()) {
-                case "blue" -> BigDecimal.valueOf(5.0 / blueRed);
-                case "red" -> BigDecimal.valueOf(3.5 / blueRed);
-                case "green" -> BigDecimal.valueOf(4.40 * blackGreen);
-                case "black" -> BigDecimal.valueOf(6.80 * blackGreen);
+                case "blue" -> BigDecimal.valueOf(5.0 / divideCards(product));
+                case "red" -> BigDecimal.valueOf(3.5 / divideCards(product));
+                case "green" -> BigDecimal.valueOf(4.40 * calculatePercentage(product));
+                case "black" -> BigDecimal.valueOf(6.80 * calculatePercentage(product));
                 default -> BigDecimal.valueOf(2.0);
             };
-        } else {
-            return product.getSellPrice();
         }
+        return null;
+    }
+
+    private static double divideCards(Product product) {
+        if (product.getAge() != null && product.getAge() >= 10 || product.isStinky()) {
+            return 2.0;
+        }
+        return 1.0;
+    }
+
+    private static double calculatePercentage(Product product) {
+        if(product.getAge() !=null && product.getAge() >= 20) {
+                return 1.2;
+        }
+        return 1.0;
     }
 
 }
