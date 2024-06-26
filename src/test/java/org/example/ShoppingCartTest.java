a2f646fd356726c4393e12ea8efce7746ad56438
MiguelAngelGSG <ma.gsg.555@gmail.com>
2024-06-26 08:33:15 +0200

    Aquatic Test

--- src/test/java/org/example/ShoppingCartTest.java
+++ src/test/java/org/example/ShoppingCartTest.java
@@ -98,24 +98,6 @@ class ShoppingCartTest {
         assertEquals(shoppingCart.getTotalPrice(), 50.0);
 
     }
-    @Test
-    void calculatePriceFishBlue() {
-       AquaticAnimal aquaticAnimal = new AquaticAnimal("blue", 0);
-        assertEquals(aquaticAnimal.calculatePrice(), 0.10);
-
-    }
-    @Test
-    void calculatePriceFishGold() {
-        AquaticAnimal aquaticAnimal = new AquaticAnimal("gold", 1);
-        assertEquals(aquaticAnimal.calculatePrice(), 100);
-
-    }
-    @Test
-    void calculatePriceFish() {
-        AquaticAnimal aquaticAnimal = new AquaticAnimal( "", 0);
-        assertEquals(aquaticAnimal.calculatePrice(), 0);
-
-    }
 
     @Test
     void calculatePriceForFourLeggedPet() {
